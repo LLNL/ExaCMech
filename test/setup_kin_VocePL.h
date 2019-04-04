6603063a26414439dff8659dd5eb06e4d199c7e7
Nathan.Barton <barton22@llnl.gov>
2019-04-04 13:30:43 -0700

    setup for stacking parameters

--- test/setup_kin_VocePL.h
+++ test/setup_kin_VocePL.h
@@ -1,10 +1,14 @@
       {
          real8 mu = 1.0, xm = 0.01, gam_w = 1.0 ;
          real8 h0 = 200e-5, tausi = 100e-5, taus0 = 400e-5, xms = 0.05, gamss0 = 1.0e-6 ;
-         std::vector<real8> params{
+         std::vector<real8> paramsThese{
             mu, xm, gam_w,
             h0, tausi, taus0, xms, gamss0,
             tausi // hdn_init
          } ;
-         kinetics.setParams( params ) ;
+#ifdef STACK_PARAMS
+         params.insert(params.end(), paramsThese.begin(), paramsThese.end()) ;
+#else
+         kinetics.setParams( paramsThese ) ;
+#endif
       }
